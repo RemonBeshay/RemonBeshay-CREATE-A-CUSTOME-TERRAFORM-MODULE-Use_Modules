8cf94a7b96c2862ca51fd03c26152e7e2e5dea91
RemonBeshay <remon.m.beshay@gmail.com>
2024-06-14 12:32:50 +0200

    Delete modules/main.tf

--- modules/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0b3aef6bc281a13b2"
-  instance_type = "t3.micro"
-  subnet_id     = "subnet-0026054bee53fc8dc"
-  tags = {
-    Name = "WayneCorp"
-  }
-}
